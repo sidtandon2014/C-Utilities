4953fc73d5d85a0c96bb706ef24f900854e6f4cd
sidtandon2014 <siddharth.tandon1@gmail.com>
2020-07-31 14:27:33 +0530

    Update Class1.cs

--- Azure Storage/AzureQueueReadingAndWriting/Common/Class1.cs
+++ Azure Storage/AzureQueueReadingAndWriting/Common/Class1.cs
@@ -9,7 +9,7 @@ namespace Common
         
         public static string getConnString()
         {
-            return "DefaultEndpointsProtocol=https;AccountName=sahpexcalibur;AccountKey=BC0dpcC+SxBgFXjh1EBuma7mN4IBXE7jVDYC23k9eN97R4OKUOCShRPDLeFLSzf97yr8R2pfV57SpGO5h5bNzA==;EndpointSuffix=core.windows.net";
+            return "DefaultEndpointsProtocol=https;AccountName=sahpexcalibur;AccountKey=;EndpointSuffix=core.windows.net";
         }
     }
 }
