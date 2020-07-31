d584025f38fb3ffe6f6a6c4e2b6e7f2a59132955
Siddharth Tandon <sitandon@microsoft.com>
2020-07-31 14:15:08 +0530

    Updated folder

---
+++ Azure Storage/AzureQueueReadingAndWriting/Common/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Common
+{
+    public static class ConnectionString
+    {
+        
+        public static string getConnString()
+        {
+            return "DefaultEndpointsProtocol=https;AccountName=sahpexcalibur;AccountKey=BC0dpcC+SxBgFXjh1EBuma7mN4IBXE7jVDYC23k9eN97R4OKUOCShRPDLeFLSzf97yr8R2pfV57SpGO5h5bNzA==;EndpointSuffix=core.windows.net";
+        }
+    }
+}
---
+++ Azure Storage/AzureQueueReadingAndWriting/QueueReader/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using Microsoft.Azure; // Namespace for CloudConfigurationManager
+using Microsoft.Azure.Storage; // Namespace for CloudStorageAccount
+using Microsoft.Azure.Storage.Queue; // Namespace for Queue storage types
+using Common;
+namespace QueueReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Retrieve storage account from connection string
+            string storageConnectionString = Common.ConnectionString.getConnString();
+                
+            // Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageConnectionString);
+
+
+            // Create the queue client
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            // Retrieve a reference to a queue
+            CloudQueue queue = queueClient.GetQueueReference("myqueue");
+
+            // Peek at the next message
+            while(true){
+                CloudQueueMessage retrievedMessage = queue.GetMessage();
+                Console.WriteLine(retrievedMessage.AsString);
+                // Process all messages in less than 5 minutes, deleting each message after processing.
+                queue.DeleteMessage(retrievedMessage);
+                Thread.Sleep(500);
+                
+            }
+            // Display message.
+            
+        }
+    }
+}
